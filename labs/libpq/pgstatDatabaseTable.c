88d7ccc50a4f2602d0d40bd424fa655fc3b7b735
Rodrigo Hjort <rodrigo.hjort@gmail.com>
2011-09-06 16:20:34 -0400

    pgstatDatabaseTable.c working

--- labs/libpq/pgstatDatabaseTable.c
+++ labs/libpq/pgstatDatabaseTable.c
@@ -19,13 +19,15 @@ main(int argc, char **argv)
 	int			i,
 				j;
 
-	const char* suffixes[] = {
+	const char* suffixes[] =
+	{
 		"Index", "Name", "Backends",
 		"Commits", "Rollbacks",
 		"BlocksRead", "BlocksHit",
 		"TuplesReturned", "TuplesFetched",
 		"TuplesInserted", "TuplesUpdated", "TuplesDeleted",
-		"RollbackRatio", "CacheHitRatio", "TuplesModified"};
+		"RollbackRatio", "CacheHitRatio", "TuplesModified"
+	};
 
 	/*
 	 * If the user supplies a parameter on the command line, use it as the
@@ -68,81 +70,14 @@ ORDER BY datid");
 
 	printf("\n");
 	nFields = PQnfields(res);
-	for (i = 0; i < PQntuples(res); i++) {
+	for (i = 0; i < PQntuples(res); i++)
+	{
 		for (j = 0; j < nFields; j++)
 			printf("pgstatDatabase%s.%d = %s\n", suffixes[j], i + 1, PQgetvalue(res, i, j));
 		printf("\n");
 	}
 
-	//printf("\n\n");
 	PQclear(res);
-	// ...
-
-	/*
-	 * Our test case here involves using a cursor, for which we must be inside
-	 * a transaction block.  We could do the whole thing with a single
-	 * PQexec() of "select * from pg_database", but that's too trivial to make
-	 * a good example.
-	 */
-
-	/* Start a transaction block */
-	/*res = PQexec(conn, "BEGIN");
-	if (PQresultStatus(res) != PGRES_COMMAND_OK)
-	{
-		fprintf(stderr, "BEGIN command failed: %s", PQerrorMessage(conn));
-		PQclear(res);
-		exit_nicely(conn);
-	}*/
-
-	/*
-	 * Should PQclear PGresult whenever it is no longer needed to avoid memory
-	 * leaks
-	 */
-	/*PQclear(res);*/
-
-	/*
-	 * Fetch rows from pg_database, the system catalog of databases
-	 */
-	/*res = PQexec(conn, "DECLARE myportal CURSOR FOR select * from pg_database");
-	if (PQresultStatus(res) != PGRES_COMMAND_OK)
-	{
-		fprintf(stderr, "DECLARE CURSOR failed: %s", PQerrorMessage(conn));
-		PQclear(res);
-		exit_nicely(conn);
-	}
-	PQclear(res);
-
-	res = PQexec(conn, "FETCH ALL in myportal");
-	if (PQresultStatus(res) != PGRES_TUPLES_OK)
-	{
-		fprintf(stderr, "FETCH ALL failed: %s", PQerrorMessage(conn));
-		PQclear(res);
-		exit_nicely(conn);
-	}*/
-
-	/* first, print out the attribute names */
-	/*nFields = PQnfields(res);
-	for (i = 0; i < nFields; i++)
-		printf("%-15s", PQfname(res, i));
-	printf("\n\n");*/
-
-	/* next, print out the rows */
-	/*for (i = 0; i < PQntuples(res); i++)
-	{
-		for (j = 0; j < nFields; j++)
-			printf("%-15s", PQgetvalue(res, i, j));
-		printf("\n");
-	}
-
-	PQclear(res);*/
-
-	/* close the portal ... we don't bother to check for errors ... */
-	/*res = PQexec(conn, "CLOSE myportal");
-	PQclear(res);*/
-
-	/* end the transaction */
-	/*res = PQexec(conn, "END");
-	PQclear(res);*/
 
 	/* close the connection to the database and cleanup */
 	PQfinish(conn);
